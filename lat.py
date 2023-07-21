56b2d703b53f1f6d7ab4bee1fe37587617a0c324
Ashley <ashleyq007@gmail.com>
2023-07-20 22:01:15 -0700

    Update lat.py

--- lat.py
+++ lat.py
@@ -1,5 +1,27 @@
-import pandas as pd
+import re, pandas as pd
 
-df = pd.read_csv("Food_Inspections.csv")
+df = pd.read_csv('Food_Inspections.csv')
 
-print(df)
+# iterate over columns and print out attribute names
+# for col in df.columns:
+	# print(col)
+
+lat_values = df['Latitude']
+long_values = df['Longitude']
+
+print("\nThere are",(len(lat_values)),"latitude records in this data set.")
+
+print("There are",(len(long_values)),"longitude records in this data set.\n")
+
+
+print("\nThere are ", lat_values.isna().sum(),"null values that need removal from the Latitude column.")
+lat_values = lat_values.dropna()
+print("\nNow there are",(len(lat_values)),"latitude records in this data set.\n")
+
+print("\nThere are ", long_values.isna().sum(),"null values that need removal from the Longitude column.")
+long_values = long_values.dropna()
+print("\nNow there are",(len(long_values)),"longitude records in this data set.\n")
+
+# check that lat values are valid, must be float within range:
+
+# check that long values are valid, must be float within range:
